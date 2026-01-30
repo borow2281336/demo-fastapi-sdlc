444d55f72454a08590bd00c77dcb96e9cb4962c1
Ivan <149667620+borow2281336@users.noreply.github.com>
2026-01-30 20:55:17 +0300

    Update base.py

--- app/model/base.py
+++ app/model/base.py
@@ -1,13 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 from sqlalchemy import DateTime, String, Column
 from sqlalchemy.sql import func
-from cuid2 import CUID
+from uuid import uuid4
 
 DeclarativeBase: DeclarativeMeta = declarative_base()
 
-
 class BaseModel(DeclarativeBase):
     __abstract__ = True
-    id = Column(String, default=CUID().generate, primary_key=True)
+    id = Column(String, default=lambda: str(uuid4()), primary_key=True)
     date_created = Column(DateTime, default=func.now())
     date_updated = Column(DateTime, default=func.now(), onupdate=func.now())
